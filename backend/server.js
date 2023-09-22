e1ba1aec2844e5e82bb227a8686affaac5178b67
Madhur Gupta <madhurrgupta@gmail.com>
2023-09-22 17:17:42 +0530

    feat: error handling

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
 const app = require("./app");
-
 const dotenv = require("dotenv");
-const connectDatabase = require('./config/db');
-dotenv.config({ path: "backend/config/config.env" });
+const connectDatabase = require("./config/db");
 
+//Uncaught Error Handling
+process.on("uncaughtException", (err) => {
+	console.log(`Error: ${err.message}`);
+	console.log(`Shutting down the server due to uncaught error`);
+
+	process.exit(1);
+});
+
+dotenv.config({ path: "backend/config/config.env" });
 connectDatabase();
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is working on http://localhost:${process.env.PORT}`);
+	console.log(`Server is working on http://localhost:${process.env.PORT}`);
+});
+
+console.log(you);
+// Unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+	console.log(`Error: ${err.message}`);
+	console.log(`Shutting down the server due to unhandled promise rejection`);
+	server.close(() => {
+		process.exit(1);
+	});
 });
