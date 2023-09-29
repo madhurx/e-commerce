38a0e97aca53ac4b7e497a4728c8ef172431fcca
Madhur Gupta <madhurmawai@gmail.com>
2023-09-29 17:58:15 +0530

    email sender

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,7 +1,7 @@
 const nodeMailer = require("nodemailer");
 
 const sendEmail = async (options) => {
-	const transporter = nodeMailer.createTransport({
+	const transporter = await nodeMailer.createTransport({
 		service: process.env.SMPT_SERVICE,
 		auth: {
 			user: process.env.SMPT_MAIL,
