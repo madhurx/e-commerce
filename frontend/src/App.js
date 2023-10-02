ea30ea115966707beee24e7a4f4e5c0d02d79e65
Madhur <madhurrgupta@gmail.com>
2023-10-02 23:40:56 +0530

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import Header from "./component/layout/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
 	return (
-		<div>Hello</div>
+        <Router>
+        <Header/></Router>
 	);
 }
 
