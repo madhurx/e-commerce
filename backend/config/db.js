d57e730dec3a1afa885570e49f65b3e8d54c7fcf
Madhur <madhurrgupta@gmail.com>
2023-10-18 00:06:30 +0530

    cloud DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-	mongoose
-		.connect(process.env.DB_URI, {
-			useNewURLParser: true,
-			useUnifiedTopology: true,
-		})
-		.then((data) => {
-			console.log(
-				`Mongodb connected with server ${data.connection.host}:${data.connection.port}/${data.connection.name}`,
-			);
-		});
+	const { MONGO_CLOUD_USERNAME, MONGO_CLOUD_PASSWORD } = process.env;
+
+	const connStr = `mongodb+srv://${MONGO_CLOUD_USERNAME}:${MONGO_CLOUD_PASSWORD}@cluster0.o7jvuqc.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=AtlasApp`;
+	const connectionParams = {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+
+	mongoose.connect(connStr, connectionParams).then((data) => {
+		console.log(
+			`Mongodb connected with server ${data.connection.host}:${data.connection.port}/${data.connection.name}`,
+		);
+	});
 };
+
 module.exports = connectDatabase;
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -77,4 +77,4 @@ const productSchema = new mongoose.Schema({
 	},
 });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model.Product || mongoose.model("Product", productSchema);
