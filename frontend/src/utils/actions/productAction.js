6cda82e31fca33f43931a97ab57870edb9e62049
Madhur Gupta <madhurrgupta@gmail.com>
2023-10-20 17:52:31 +0530

    product details

--- frontend/src/utils/actions/productAction.js
+++ frontend/src/utils/actions/productAction.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getProducts = createAsyncThunk("allProductsSlice", async () => {
 	try {
-		const data = await fetch("api/v1/products/");
+		const data = await fetch("/api/v1/products/");
 		const result = await data.json();
 		return result;
 	} catch (error) {
@@ -12,9 +12,8 @@ export const getProducts = createAsyncThunk("allProductsSlice", async () => {
 
 export const getProductDetail = createAsyncThunk("productDetailSlice", async (id) => {
     try {
-		const data = await fetch(`api/v1/product/${id}`);
+		const data = await fetch(`/api/v1/product/${id}`);
 		const result = await data.json();
-        console.log(result);
 		return result;
 
 
