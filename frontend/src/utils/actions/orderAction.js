28f2233549c50e370fa82c0b7852d3ee3c821ba9
Madhur <madhurrgupta@gmail.com>
2023-12-05 15:13:13 +0530

    feat: Payment

--- frontend/src/utils/actions/orderAction.js
+++ frontend/src/utils/actions/orderAction.js
@@ -1,4 +1,20 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const createOrder = createAsyncThunk("newOrderSlice", async (newOrderActionParams) => {
-});
+export const createOrder = createAsyncThunk(
+	"newOrderSlice",
+	async (order, { dispatch }) => {
+		try {
+			const config = {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			};
+			const { data } = await axios.post("/api/v1/order/new", order, config);
+			const result = await data.json();
+			return result;
+		} catch (error) {
+			console.log(error);
+		}
+	},
+);
--- frontend/src/utils/slices/orderSlice.js
+++ frontend/src/utils/slices/orderSlice.js
@@ -1,14 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createOrder } from "../actions/orderAction";
 
 const orderSlice = createSlice({
 	name: "cart",
-	initialState: {},
-	reducers: {clearErrors: (state) => {
-        state.error = null;
-        return state;
-    },},
-    extraReducers:{},
+	initialState: { loading: true, order: {}, error: null },
+	reducers: {
+		clearErrors: (state) => {
+			state.error = null;
+			return state;
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(createOrder.pending, (state, action) => {
+				state.loading = true;
+				return state;
+			})
+			.addCase(createOrder.fulfilled, (state, action) => {
+				state.loading = false;
+				state.order = action.payload;
+			})
+			.addCase(createOrder.rejected, (state, action) => {
+				state.loading = false;
+				state.error = action.payload;
+			})
+			.addDefaultCase((state) => state);
+	},
 });
 
-export const {clearErrors  } = orderSlice.actions;
+export const { clearErrors } = orderSlice.actions;
 export default orderSlice.reducer;
